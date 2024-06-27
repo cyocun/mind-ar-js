b04d7de980d898e375ee89eddc1dfd81033305d6
cyocun <cyocun@gmail.com>
2024-06-27 14:11:28 +0900

    dist含めるように変更

---
+++ dist/mindar-face.prod.js
@@ -0,0 +1,12 @@
+import { C as o } from "./controller-DeThRbZw.js";
+import { U as r } from "./ui-D7R2QPpe.js";
+const i = {
+  Controller: o,
+  UI: r
+};
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.FACE = i;
+export {
+  o as Controller,
+  r as UI
+};
---
+++ dist/mindar-image.prod.js
@@ -0,0 +1,13 @@
+import { C as o, a as r } from "./controller-CZj5QYav.js";
+import { U as i } from "./ui-D7R2QPpe.js";
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.IMAGE = {
+  Controller: o,
+  Compiler: r,
+  UI: i
+};
+export {
+  r as Compiler,
+  o as Controller,
+  i as UI
+};
